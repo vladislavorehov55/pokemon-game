4daf2d4d8a0861c6c71ec6bfaaee5b6566513f0f
vladislavorehov55 <vladislavorehov55@gmail.com>
2021-01-28 21:47:52 +0300

    Homework

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World, ReactJS!</p>
-
-      </header>
-    </div>
-  );
-}
+import Header from "./components/header/Header";
+import Layout from "./components/layout/Layout";
+import Footer from "./components/Footer/Footer";
+import bg from './images/bg2.jpg'
+const App = () => {
+    return (
+        <>
+            <Header title='This is title' descr='This is Description!'/>
+            <Layout id={1} title={'Layout'} descr={'First layout'} urlBg={bg} />
+            <Layout id={2} title={'Layout'} descr={'Second layout'} colorBg={'red'}/>
+            <Layout id={3} title={'Layout'} descr={'Third layout'} urlBg={bg}/>
+            <Footer/>
+        </>
+    )
+};
 
 export default App;
